71aef8fac386ba6955954e134854fdfe52de0ec5
frabru99 <brunellofraancesco5@gmail.com>
2023-05-30 22:44:23 +0200

    ok

---
+++ src/main/java/Ciao/Testciao.java
@@ -0,0 +1,10 @@
+package Ciao;
+
+
+
+public class Testciao {
+
+	public static void main(String[] args) {
+		System.out.println("Ciao totore!!!!!");
+	}
+}
---
+++ src/test/java/com/ok/TestGit/ApplicationTest.java
@@ -0,0 +1,12 @@
+package com.ok.TestGit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
